4890738ddcb66f1de93c73292ac13e427445f01a
Karen Payne <Karen.1.Payne@oregon.gov>
2021-02-07 07:00:16 -0800

    Added group radio button, readme update

---
+++ TeamControls/RadioGroupBox.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TeamControls
+{
+    /// <summary>
+    /// See https://github.com/karenpayneoregon/csharp-features/tree/master/RadioButtonBinding for usage
+    /// (Note the link above uses a regular RadioButton while this code uses <see cref="RadioButtonCustom"/> which is a simple change in the implementation)
+    /// </summary>
+    [DefaultBindingProperty("Selected")]
+    public class RadioGroupBox : GroupBox
+    {
+        [Description("Occurs when the selected value changes.")]
+        public event SelectedChangedEventHandler SelectedChanged;
+
+        public class SelectedChangedEventArgs : EventArgs
+        {
+            public int Selected { get; private set; }
+
+            internal SelectedChangedEventArgs(int selected)
+            {
+                Selected = selected;
+            }
+        }
+
+        public delegate void SelectedChangedEventHandler(object sender, SelectedChangedEventArgs e);
+
+
+        private int _selected;
+
+        [Browsable(false)]
+        [Bindable(BindableSupport.Yes, BindingDirection.TwoWay)]
+        [Description("The selected value associated with this control."), Category("Data")]
+        public int Selected
+        {
+            get => _selected;
+            set
+            {
+
+                var radioButton = this.Controls.OfType<RadioButtonCustom>()
+                    .FirstOrDefault(radio => radio.HasId);
+
+                if (radioButton == null) return;
+                radioButton.Checked = true;
+                _selected = radioButton.Id;
+            }
+        }
+
+        protected override void OnControlAdded(ControlEventArgs e)
+        {
+            base.OnControlAdded(e);
+
+            if (e.Control is RadioButton radioButton)
+            {
+                radioButton.CheckedChanged += radioButton_CheckedChanged;
+            }
+        }
+
+        protected void OnSelectedChanged(SelectedChangedEventArgs e)
+        {
+            SelectedChanged?.Invoke(this, e);
+        }
+
+        private void radioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            var radio = (RadioButtonCustom)sender;
+
+            if (radio.Checked && radio.Tag != null && int.TryParse(radio.Tag.ToString(), out var val))
+            {
+                _selected = val;
+                OnSelectedChanged(new SelectedChangedEventArgs(_selected));
+            }
+        }
+    }
+}
--- TeamControls/readme.md
+++ TeamControls/readme.md
@@ -1,3 +1,23 @@
 ﻿# About
 
 Sample windows form control library for upcoming TechNet article.
+
+# Controls
+
+Useful controls to use in Windows Forms projects.
+
+- Id property is to store a numeric value that might normally be stored in the Tag property of a normal control.
+- HasId, example to check if an identifier is set
+- Stash, used to store string information for the control. This concept can be extended to hold other types of data.
+- Control names can be changed to suit a developer needs and is encouraged.
+
+| Control  | &nbsp; | Description  |
+|:-------------|:---|
+| CheckBoxCustom  | &nbsp; | Properties, Id property, Stash   |
+| DateTimePickerCustom | &nbsp; | Properties, Id property, Stash, set default format and size of the control.   |
+| IntegerTextBox  | &nbsp; | Properties, Id property, Stash, simple text box to allow only integer values   |
+| NoBeepTextBox  | &nbsp; | Properties, Id property, Stash, pressing <kbd>enter</kbd> key will not cause a beep sound   |
+| PictureBoxCustom  | &nbsp; | Properties, Id property, Stash, method to export image to file.   |
+| RadioButtonCustom  | &nbsp; | Properties, Id property, Stash, method to export image to file.   |
+
+See [source code](https://github.com/karenpayneoregon/team-controls/tree/master/TeamControls).
