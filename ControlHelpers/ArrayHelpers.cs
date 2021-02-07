41e03199605a1ccdb898bb5f3ec9f1c7ae93a8c1
Karen Payne <Karen.1.Payne@oregon.gov>
2021-02-07 15:02:59 -0800

    Array helper class

---
+++ ControlHelpers/ArrayHelpers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamHelpers
+{
+    public class ArrayHelpers
+    {
+        /// <summary>
+        /// Concatenate arrays
+        /// </summary>
+        /// <typeparam name="T">type e.g. int, string etc</typeparam>
+        /// <param name="sender">data</param>
+        /// <returns>an array of T</returns>
+        public static T[] ConcatArrays<T>(params T[][] sender)
+        {
+            var position = 0;
+            var outputArray = new T[sender.Sum(a => a.Length)];
+            
+            foreach (var current in sender)
+            {
+                Array.Copy(current, 0, outputArray, position, current.Length);
+
+                position += current.Length;
+            }
+            
+            return outputArray;
+        }
+    }
+}
